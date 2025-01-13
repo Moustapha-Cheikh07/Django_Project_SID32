6f2042646113a15f0aa6cdef22da7dcbf0831aed
Moustapha-Cheikh07 <23007@esp.mr>
2025-01-13 20:34:38 +0000

    Update Project

--- ansade/ansade/settings.py
+++ ansade/ansade/settings.py
@@ -60,9 +60,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # Utiliser MySQL comme backend
         'NAME': 'Inpc_db',  # Nom de la base de données
-        'USER': 'root',  # Nom de l'utilisateur MySQL
+        'USER': 'django',  # Nom de l'utilisateur MySQL
         'PASSWORD': 'Medmouna',  # Mot de passe de l'utilisateur MySQL
-        'HOST': '127.0.0.1',  # Utiliser localhost si le serveur MySQL est local
+        'HOST': 'db',  # Utiliser localhost si le serveur MySQL est local
         'PORT': '3306',  # Le port MySQL par défaut
     }
 }
